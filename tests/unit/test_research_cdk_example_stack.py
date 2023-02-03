a57ee0eb0fea97c7507a39397659a4441cb45b86
Tim Cutts <tjrc@amazon.com>
2023-02-03 16:52:38 +0000

    Add some example tests

--- tests/unit/test_research_cdk_example_stack.py
+++ tests/unit/test_research_cdk_example_stack.py
@@ -3,13 +3,20 @@ import aws_cdk.assertions as assertions
 
 from research_cdk_example.research_cdk_example_stack import ResearchCdkExampleStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in research_cdk_example/research_cdk_example_stack.py
-def test_sqs_queue_created():
+# example tests. To run these tests, install the dev requirements
+# and then run "pytest" in the project root
+
+def test_resources_created():
     app = core.App()
     stack = ResearchCdkExampleStack(app, "research-cdk-example")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.resource_count_is("AWS::S3::Bucket", 2)
+
+    template.has_resource_properties("AWS::SNS::Subscription", {
+        "Protocol": "email"
+    })
+
+    template.has_resource_properties("AWS::Batch::JobDefinition", {
+        "Type": "container"
+    })
